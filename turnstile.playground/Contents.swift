2f1ff8ba0f208bf961fe185a8adfdaee57735fb0
GangWoon <gangwoon2018@gmail.com>
2024-09-06 02:47:14 +0900

    Step 4

--- turnstile.playground/Contents.swift
+++ turnstile.playground/Contents.swift
@@ -1,18 +1,7 @@
 import Foundation
 
-Turnstile<Locked>()
-  .insertCoin()
-  .push()
-  .insertCoin()
-  .push()
-
-var locked = Turnstile<Locked>()
-
-/// Aliasing
-var unlocked = locked.insertCoin()
-var unlocked2 = unlocked
-
-/// ↓
-/// The code below may cause aliasing issues because both `unlocked` and `unlocked2` reference the same state.
-/// Modifications to one variable may inadvertently affect the other, leading to unintended side effects.
+var locked = TurnstileState()
+locked.insertCoin()
+locked.push()
+locked.coins()
 
